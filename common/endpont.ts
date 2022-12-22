c279ce77efd436fc1ddba033db8c74769c016b31
Hoàng Quân <hoangquan225.qh@gmail.com>
2022-12-22 23:04:06 +0700

    update end point

--- common/endpont.ts
+++ common/endpont.ts
@@ -1,5 +1,11 @@
 export default class ENDPONTAPI {
     static LOGIN = '/login';
-    static REGISTER = '/register'; 
+    static REGISTER = '/register';
     static LOGOUT = '/logout';
+
+    static UPDATE_USER = '/update-user'
+    static LOAD_USER_INFO = "/load-user-info";
+    static UPLOAD = "/upload";
+
+    static GET_USER_FROM_TOKEN = "/get-user-from-token";
 }
