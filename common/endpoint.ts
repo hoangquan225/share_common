57c6206a1061aa48d5f0a7315fd8bbc80dda309c
Hoàng Quân <hoangquan225.qh@gmail.com>
2022-12-30 01:08:35 +0700

    cap nhat lai endpoint

--- common/endpoint.ts
+++ common/endpoint.ts
@@ -15,4 +15,24 @@ export default class ENDPONTAPI {
   // category 
   static GET_CATEGORYS_BY_STATUS = '/category/load-category-by-status'
   static UPDATE_CATEGORY = '/category/update-category'
+
+  // course 
+  static GET_COURSES_BY_STATUS = '/course/load-course-by-status'
+  static UPDATE_COURSE = '/course/update-course'
+
+  // lesson 
+  static GET_LESSONS_BY_STATUS = '/lesson/load-lesson-by-status'
+  static UPDATE_LESSON = '/lesson/update-lesson'
+
+  // question 
+  static GET_QUESTIONS_BY_STATUS = '/question/load-question-by-status'
+  static UPDATE_QUESTION = '/question/update-question'
+
+  // tag 
+  static GET_TAGS_BY_STATUS = '/tag/load-tag-by-status'
+  static UPDATE_TAG = '/tag/update-category'
+
+  // topic 
+  static GET_TOPICS_BY_STATUS = '/topic/load-topic-by-status'
+  static UPDATE_TOPIC = '/topic/update-topic'
 }
