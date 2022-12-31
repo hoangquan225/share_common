01500c1738b5c77ce9513367714d3d8001c94738
Hoàng Quân <hoangquan225.qh@gmail.com>
2022-12-31 13:36:09 +0700

    update DB category

--- models/category.ts
+++ models/category.ts
@@ -2,6 +2,8 @@ class Category {
     id: string | undefined; 
     name: string;
     status: number;
+    avatar?: string;
+    des: string;
     slug: string;
     createDate ?: number;
     updateDate ?: number;
@@ -10,6 +12,8 @@ class Category {
         this.id = args?._id ?? undefined;
         this.name = args?.name ?? '';
         this.status = args?.status ?? 0;
+        this.avatar = args?.avatar ?? "";
+        this.des = args?.des ?? '';
         this.slug = args?.slug ?? '';
         this.createDate = args?.createDate ?? undefined; 
         this.updateDate = args?.updateDate ?? undefined;
