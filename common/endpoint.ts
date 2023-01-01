68996de2de3f7b39ae24d01665118d06fa5918c9
Hoàng Quân <hoangquan225.qh@gmail.com>
2023-01-02 01:02:47 +0700

    update endpoint

--- common/endpoint.ts
+++ common/endpoint.ts
@@ -21,6 +21,7 @@ export default class ENDPONTAPI {
   static GET_COURSES_BY_STATUS = '/course/load-course-by-status'
   static UPDATE_COURSE = '/course/update-course'
   static GET_COURSES_BY_ID_CATEGORY = '/course/load-course-by-id-category'
+  static GET_COURSES_BY_ID_TAG_AND_CATEGORY = '/course/load-course-by-id-tag-and-category'
 
   // lesson 
   static GET_LESSONS_BY_STATUS = '/lesson/load-lesson-by-status'
