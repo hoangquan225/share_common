15ceeaa58f8be0ca59f5e5082f178c5f813a5dbe
Hoàng Quân <hoangquan225.qh@gmail.com>
2023-01-02 18:46:51 +0700

    update endpoint

--- common/endpoint.ts
+++ common/endpoint.ts
@@ -34,6 +34,7 @@ export default class ENDPONTAPI {
   // tag 
   static GET_TAGS_BY_STATUS = '/tag/load-tag-by-status'
   static UPDATE_TAG = '/tag/update-tag'
+  static GET_TAGS_BY_ID_CATEGORY = '/tag/load-tag-by-id-category'
 
   // topic 
   static GET_TOPICS_BY_STATUS = '/topic/load-topic-by-status'
