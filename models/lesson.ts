1f69073b0f5a79944c610319896470a7c2bd8ad2
Nguyen Duy Hung <duyhungfa102@gmail.com>
2023-01-28 22:07:23 +0700

    update lesson

--- models/lesson.ts
+++ models/lesson.ts
@@ -1,11 +1,16 @@
+import { isObject } from "../utils/validation";
+import { Topic } from "./topic";
+
 class Lesson {
     id?: string | undefined;
     status: number;
     idTopic : string | undefined;
     video?: string;
-    createDate ?: number; 
+    createDate ?: number;
     updateDate ?: number;
     des : string;
+    time: number | null;
+    dataTopic: Topic | null;
     type : number | null; // 1: video , 2: document
 
     constructor(args?: any) {
@@ -16,6 +21,9 @@ class Lesson {
         this.createDate = args?.createDate ?? undefined; 
         this.updateDate = args?.updateDate ?? undefined;
         this.des = args?.des ?? '';
+        this.time = args?.time ?? null;
+        this.idTopic = isObject(args.idTopic) ? new Topic(args.idTopic)?.id : (args?.idTopic ?? undefined);
+        this.dataTopic = isObject(args.idTopic) ? new Topic(args.idTopic) : null; 
         this.type = args?.type ?? null;
     }
 
