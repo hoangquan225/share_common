3dc68a2ab481926e71e87bce4cb8f2318dea0625
Hoàng Quân <hoangquan225.qh@gmail.com>
2023-02-03 12:11:03 +0700

    add score in model topic

--- models/topic.ts
+++ models/topic.ts
@@ -1,41 +1,51 @@
 import { isObject } from "../utils/validation";
 
-class Topic { 
-    id?: string | undefined; 
-    name: string; 
-    status: number; 
-    idCourse: string; 
-    topicChild: string[];
-    topicChildData: Topic[];
-    parentId: string | null;
-    type: number; // type = 1 chương trình học , type = 2 đề kiểm tra
-    topicType: number | null; // 1 : chuong, 2 : bài tập, 3: đề kiểm tra, 4: video, 5: document
-    timeExam ?: number; // thời gian làm bài kiểm tra
-    video ?: string | null; //video bai giang
-    numQuestion ?: number;
-    des: string;
-    index: number;
-    createDate ?: number;
-    updateDate ?: number;
+class Topic {
+  id?: string | undefined;
+  name: string;
+  status: number;
+  idCourse: string;
+  topicChild: string[];
+  topicChildData: Topic[];
+  parentId: string | null;
+  type: number; // type = 1 chương trình học , type = 2 đề kiểm tra
+  topicType: number | null; // 1 : chuong, 2 : bài tập, 3: đề kiểm tra, 4: video, 5: document
+  timeExam?: number; // thời gian làm bài kiểm tra
+  video?: string | null; //video bai giang
+  numQuestion?: number;
+  des: string;
+  index: number;
+  score: number | null;
+  createDate?: number;
+  updateDate?: number;
 
-    constructor(args?:any) {
-        this.id = args?._id ?? args?.id ?? undefined;
-        this.name = args?.name ?? ''; 
-        this.status = args?.status ?? 0; 
-        this.idCourse = args?.idCourse ?? '';
-        this.topicChild = args.topicChild ? (isObject(args.topicChild[0]) ? args.topicChild?.map((o: any) => o._id ?? '') : args?.topicChild) : [] ;
-        this.topicChildData = args.topicChild ? (isObject(args.topicChild[0]) ? args.topicChild?.map((o: any) => new Topic(o)) : []) : []
-        this.parentId = args?.parentId ?? null;
-        this.type = args?.type ?? 1;
-        this.des = args?.des ?? '';
-        this.index = args?.index ?? 0;
-        this.createDate = args?.createDate ?? undefined; 
-        this.updateDate = args?.updateDate ?? undefined;
-        this.topicType = args?.topicType ?? null;
-        this.timeExam = args?.timeExam ?? 0;
-        this.numQuestion = args?.numQuestion ?? 0;
-        this.video = args?.video ?? null;
-    }
+  constructor(args?: any) {
+    this.id = args?._id ?? args?.id ?? undefined;
+    this.name = args?.name ?? "";
+    this.status = args?.status ?? 0;
+    this.idCourse = args?.idCourse ?? "";
+    this.topicChild = args.topicChild
+      ? isObject(args.topicChild[0])
+        ? args.topicChild?.map((o: any) => o._id ?? "")
+        : args?.topicChild
+      : [];
+    this.topicChildData = args.topicChild
+      ? isObject(args.topicChild[0])
+        ? args.topicChild?.map((o: any) => new Topic(o))
+        : []
+      : [];
+    this.parentId = args?.parentId ?? null;
+    this.type = args?.type ?? 1;
+    this.des = args?.des ?? "";
+    this.index = args?.index ?? 0;
+    this.score = args?.score ?? null;
+    this.createDate = args?.createDate ?? undefined;
+    this.updateDate = args?.updateDate ?? undefined;
+    this.topicType = args?.topicType ?? null;
+    this.timeExam = args?.timeExam ?? 0;
+    this.numQuestion = args?.numQuestion ?? 0;
+    this.video = args?.video ?? null;
+  }
 }
 
 export { Topic };
