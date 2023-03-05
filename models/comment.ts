e64e54a744d8a20da2e94562b7923152ffbcb2ae
Nguyen Duy Hung <duyhungfa102@gmail.com>
2023-03-06 00:39:21 +0700

    update model comment

--- models/comment.ts
+++ models/comment.ts
@@ -7,21 +7,21 @@ class Comment {
     idTopic: string | undefined;
     idUser: string | undefined;
     content: string;
-    react: {
-        type: number, 
+    react?: {
+        type: number,
         idUser: string
     }[];
-    status: number;
-    index: number;
-    userInfo: UserInfo | null;
-    createDate: number;
-    updateDate: number;
+    status?: number;
+    index?: number;
+    userInfo?: UserInfo | null;
+    createDate?: number;
+    updateDate?: number;
     constructor(args?: any) {
         this.id = args?._id ?? args?.id ?? undefined;
-        this.idTopic = args?.idTopic ?? null; 
+        this.idTopic = args?.idTopic ?? null;
         this.idUser = isObject(args.idUser) ? new UserInfo(args.idUser)?._id : (args?.idUser ?? undefined);
-        this.userInfo = isObject(args.idUser) ? new UserInfo(args.idUser) : null; 
-        this.content = args?.content ?? ''; 
+        this.userInfo = isObject(args.idUser) ? new UserInfo(args.idUser) : null;
+        this.content = args?.content ?? '';
         this.react = args?.react ?? [];
         this.status = args?.status ?? TTCSconfig.STATUS_PUBLIC;
         this.index = args?.index ?? 0;
